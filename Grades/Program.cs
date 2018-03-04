958d541a4058b74b45fbae88a5a43398c0942a7b
Mateusz Małolepszy <mateusz.malolepszy@gmail.com>
2018-03-04 18:08:46 +0100

    commit fix

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -14,14 +14,14 @@ namespace Grades
             GradeBook g2 = g1;
 
             g1.Name = "Scott's grade book";
-            System.Console.WriteLine(g2.Name);
+            Console.WriteLine(g2.Name);
             /* GradeBook book = new GradeBook();
             book.AddGrade(94);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.AverageGrade)6;
             Console.WriteLine(stats.HighestGrade);
             Console.WriteLine(stats.LowestGrade);
             Console.ReadLine(); */
